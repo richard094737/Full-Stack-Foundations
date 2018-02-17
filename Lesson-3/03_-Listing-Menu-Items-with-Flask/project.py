e1190e1cf3a2fb598e0542d1806cfa2a680b30eb
richard-rainbow <richard-rainbow@hotmail.com>
2018-02-17 15:45:05 +0800

    Update project.py

--- Lesson-3/03_-Listing-Menu-Items-with-Flask/project.py
+++ Lesson-3/03_-Listing-Menu-Items-with-Flask/project.py
@@ -1,32 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Restaurant, MenuItem
+from database_setup import Base, MenuItem, Restaurant
 
 app = Flask(__name__)
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurant.db')
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-@app.route('/')
-@app.route('/hello')
-def HelloWorld():
-    restaurant = session.query(Restaurant).first()
+@app.route("/")
+@app.route("/restaurant/<int:restaurant_id>/")
+def restaurant_menu(restaurant_id):
+    restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
-    output = ''
-    for i in items:
-        output += i.name
-        output += '</br>'
-        output += i.price
-        output += '</br>'
-        output += i.description
-        output += '</br>'
-        output += '</br>'
-    return output
+    return render_template('menu.html', restaurant=restaurant, items=items)
+
+
+@app.route('/restaurant/<int:restaurant_id>/new/')
+def new_menu_item(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_item_id>/edit/')
+def edit_menu_item(restaurant_id, menu_item_id):
+    return "page to edit a menu item. Task 2 complete!"
+
+
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_item_id>/delete/')
+def delete_menu_item(restaurant_id, menu_item_id):
+    return "page to delete a menu item. Task 3 complete!"
+
 
 if __name__ == '__main__':
     app.debug = True
